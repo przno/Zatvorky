9112430df1e2c352ac745f8df4d6018c9a66ca8a
Martin Praznovsky <martin.praznovsky@gmail.com>
2017-04-05 19:35:37 +0200

    no summary
    no description

---
+++ Zatvorky/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Zatvorky
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // manualne testuj tu ale radsej si kukni unit test
+            Console.WriteLine(FindPosition("(())()))(((())((()()()))"));
+            Console.ReadKey();
+        }
+
+        public static int FindPosition(string str)
+        {
+            int index_left = 0;
+            int index_rigth = str.Length;
+            bool moveLeftToRight = true;
+
+            for (; index_left != index_rigth;)
+            {
+                Console.WriteLine("DEBUG: index_left={0}, index_right={1}", index_left, index_rigth);
+                
+                if (moveLeftToRight)
+                {
+                    if(str[index_left] == '(')
+                    {
+                        moveLeftToRight = !moveLeftToRight;
+                        index_rigth--;
+                    }
+                    else
+                    {
+                        index_left++;
+                    }
+                }
+                else
+                {
+                    if (str[index_rigth] == ')')
+                    {
+                        moveLeftToRight = !moveLeftToRight;
+                        index_left++;
+                    }
+                    else
+                    {
+                        index_rigth--;
+                    }
+                }
+            }
+
+            return index_left;
+        }
+    }
+}
---
+++ ZatvorkyTests/ProgramTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zatvorky.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void FindPositionTest()
+        {
+            Assert.AreEqual(0, Program.FindPosition(""));
+
+            Assert.AreEqual(1, Program.FindPosition("()"));
+            Assert.AreEqual(2, Program.FindPosition("(())"));
+            Assert.AreEqual(3, Program.FindPosition("((()))"));
+            Assert.AreEqual(4, Program.FindPosition("(((())))"));
+            Assert.AreEqual(5, Program.FindPosition("((((()))))"));
+
+            Assert.AreEqual(1, Program.FindPosition("()"));
+            Assert.AreEqual(2, Program.FindPosition("()()"));
+            Assert.AreEqual(3, Program.FindPosition("()()()"));
+            Assert.AreEqual(4, Program.FindPosition("()()()()"));
+            Assert.AreEqual(5, Program.FindPosition("()()()()()"));
+
+            Assert.AreEqual(0, Program.FindPosition("("));
+            Assert.AreEqual(0, Program.FindPosition("(("));
+            Assert.AreEqual(0, Program.FindPosition("((("));
+            Assert.AreEqual(0, Program.FindPosition("(((("));
+            Assert.AreEqual(0, Program.FindPosition("((((("));
+
+            Assert.AreEqual(1, Program.FindPosition(")"));
+            Assert.AreEqual(2, Program.FindPosition("))"));
+            Assert.AreEqual(3, Program.FindPosition(")))"));
+            Assert.AreEqual(4, Program.FindPosition("))))"));
+            Assert.AreEqual(5, Program.FindPosition(")))))"));
+
+            Assert.AreEqual(1, Program.FindPosition(")("));
+            Assert.AreEqual(2, Program.FindPosition(")()("));
+            Assert.AreEqual(3, Program.FindPosition(")()()("));
+            Assert.AreEqual(4, Program.FindPosition(")()()()("));
+            Assert.AreEqual(5, Program.FindPosition(")()()()()("));
+
+
+            Assert.AreEqual(1, Program.FindPosition(")"));
+            Assert.AreEqual(1, Program.FindPosition(")("));
+            Assert.AreEqual(1, Program.FindPosition(")(("));
+            Assert.AreEqual(1, Program.FindPosition(")((("));
+            Assert.AreEqual(1, Program.FindPosition(")(((("));
+            Assert.AreEqual(1, Program.FindPosition(")((((("));
+
+            Assert.AreEqual(2, Program.FindPosition("))"));
+            Assert.AreEqual(2, Program.FindPosition("))("));
+            Assert.AreEqual(2, Program.FindPosition("))(("));
+            Assert.AreEqual(2, Program.FindPosition("))((("));
+            Assert.AreEqual(2, Program.FindPosition("))(((("));
+            Assert.AreEqual(2, Program.FindPosition("))((((("));
+
+            Assert.AreEqual(3, Program.FindPosition(")))"));
+            Assert.AreEqual(3, Program.FindPosition(")))("));
+            Assert.AreEqual(3, Program.FindPosition(")))(("));
+            Assert.AreEqual(3, Program.FindPosition(")))((("));
+            Assert.AreEqual(3, Program.FindPosition(")))(((("));
+            Assert.AreEqual(3, Program.FindPosition(")))((((("));
+
+            Assert.AreEqual(5, Program.FindPosition(")())()((()(("));
+            Assert.AreEqual(6, Program.FindPosition(")())()((()()("));
+            Assert.AreEqual(7, Program.FindPosition(")())()((()()()"));
+
+        }
+    }
+}
